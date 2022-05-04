c7526b47ff43a00260069f1a9e8dccd7721900a0
Dylan Nelson <nlsdyl001@myuct.ac.za>
2022-05-04 20:15:19 +0200

    lexicographic approach

--- interlacing-triangles.cpp
+++ interlacing-triangles.cpp
@@ -1,68 +1,72 @@
+#include <algorithm>
 #include <iostream>
+#include <tuple>
 #include <vector>
 
 using namespace std;
 
-bool next_candidate(int rows, vector<vector<int>>& value_by_position, vector<pair<int, int>>& position_by_value, vector<int>& above) {
+pair<int, int> find_move(
+    int max_row,
+    int to_move,
+    const vector<vector<int>>& value_by_position,
+    const vector<pair<int, int>>& position_by_value
+) {
+    const auto [row, col] = position_by_value[to_move - 1];
+    for (int new_col = col - 1; new_col >= 0; --new_col) {
+        if (value_by_position[row][new_col] > to_move) continue;
+        if (row == max_row) {
+            return pair<int, int>{row, new_col};
+        }
+        else {
+            if (value_by_position[row + 1][new_col] > to_move) return pair<int, int>{row, new_col};
+            if (value_by_position[row + 1][new_col + 1] > to_move) return pair<int, int>{row, new_col}; 
+        }
+    }
+
+    for (int new_row = row - 1; new_row >= 0; --new_row) {
+        for (int new_col = new_row; new_col >= 0; --new_col) {
+            if (value_by_position[new_row][new_col] > to_move) continue;
+            if (value_by_position[new_row + 1][new_col] > to_move) return pair<int, int>{new_row, new_col};
+            if (value_by_position[new_row + 1][new_col + 1] > to_move) return pair<int, int>{new_row, new_col};
+        }
+    }
+
+    return pair<int, int>{-1, -1};
+}
+
+bool next_candidate(
+    int rows,
+    vector<vector<int>>& value_by_position,
+    vector<pair<int, int>>& position_by_value
+) {
     const int max_num = (rows * (rows + 1)) / 2;
-    bool move_found = false;
+    int row = -1, col = -1;
     int to_move = 1;
-    int move_i = -1, move_j = -1, move_above = -1;
+    while (to_move < max_num && row == -1) {
+        ++to_move;
+        tie(row, col) = find_move(rows - 1, to_move, value_by_position, position_by_value);
+    }
+    if (row == -1) return false;
 
-    while (to_move <= max_num && !move_found) {
-        auto [i, j] = position_by_value[to_move - 1];
-        if (i == rows) {
-           for (int new_j = j - 1; j >= 0; --j) {
-               if (value_by_position[i - 1][new_j - 1] < to_move) {
-                    move_i = i;
-                    move_j = new_j;
-                    move_above = max_num + 1;
-                    move_found = true;
-                    break;
-               } 
-           }
-        }
-        if (move_found) break;
+    vector<pair<int, int>> positions;
+    for (int i = 1; i <= to_move; ++i) {
+        const pair<int, int> pos = position_by_value[i - 1];
+        if (pos.first == row && pos.second == col) continue;
+        positions.push_back(pos);
+    }
 
-        if (i < rows) {
-            auto [above_i, above_j] = position_by_value[above[to_move - 1] - 1];
-            if (above_j == j - 1) {
-                if (j < i - 1 && value_by_position[i - 2][j] < to_move) {
-                   move_i = i - 1; 
-                   move_j = j + 1;
-                   move_above = above[to_move - 1];
-                   move_found = true;
-                }
-            }
-        }
-        if (move_found) break;
+    sort(positions.begin(), positions.end());
 
-        for (int candidate_above = min(max_num, above[to_move - 1]); candidate_above > to_move; --candidate_above) {            
-            auto [above_i, above_j] = position_by_value[candidate_above - 1];
-            if (above_i == 1) continue;
-            if (above_j > 1 && value_by_position[above_i - 2][above_j - 2] < to_move) {
-                move_i = above_i - 1;
-                move_j = above_j - 1;
-                move_above = candidate_above;
-                move_found = true;
-                break;
-            }
-            if (above_j < above_i && value_by_position[above_i - 2][above_j - 1] < to_move) {
-                move_i = above_i - 1;
-                move_j = above_j;
-                move_above = candidate_above;
-                move_found = true;
-                break;
-            }
-        }
-    } 
-    if (to_move == max_num && !move_found) return false; 
-
-
+    value_by_position[row][col] = to_move;
+    position_by_value[to_move - 1] = pair<int, int>{row, col};
+    for (int num = to_move - 1; num >= 1; --num) {
+        const auto [num_row, num_col] = positions[num];
+        value_by_position[num_row][num_col] = num;
+        position_by_value[num - 1] = positions[num];
+    }
 
     return true;
 }
-
 
 bool valid(vector<vector<int>> candidate) {
     for (int i = 0; i < candidate.size() - 1; ++i) {
@@ -78,4 +82,27 @@ bool valid(vector<vector<int>> candidate) {
 int main() {
     cout << "Enter the number of rows in the triangle: ";
     int n; cin >> n;
+
+    vector<vector<int>> value_by_position;
+    vector<pair<int, int>> position_by_value;
+    int num = 1;
+    for (int row = 0; row < n; ++row) {
+        value_by_position.push_back(vector<int>());
+        for (int col = 0; col <= row; ++col) {
+            value_by_position[row].push_back(num);
+            ++num;
+            position_by_value.push_back(pair<int, int>{row, col});
+        }
+    }
+
+    long long int num_triangles = 0;
+    if (valid(value_by_position)) ++num_triangles;
+
+    while (next_candidate(n, value_by_position, position_by_value)) {
+        cout << num_triangles << endl;
+        if (valid(value_by_position)) ++num_triangles;
+    }
+
+    cout << "Number of triangles: " << num_triangles << endl;
+
 }
