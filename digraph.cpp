a15068b5776a19abf6781c37bdf28d34afdbb154
Dylan Nelson <nlsdyl001@myuct.ac.za>
2022-05-09 01:24:29 +0200

    reimplement the algorithm used by Sage for calculating the number of linear extensions of a digraph (i.e. DP on upsets)

--- digraph.cpp
+++ digraph.cpp
@@ -1,8 +1,11 @@
+#include <algorithm>
 #include <atomic>
 #include <boost/multiprecision/cpp_int.hpp>
 #include <future>
 #include <iostream>
+#include <set>
 #include <thread>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
@@ -41,6 +44,123 @@ void update_indegrees(int num_rows, int row, int col, bool increment, uint64_t d
         }
     }
 }
+
+bool find_linear_extension(int num_rows, uint64_t digraph, int num_visited, bool visited[10][10], int in_degree[10][10], int linear_extension[100]) {
+    if (num_visited == ((num_rows * (num_rows + 1)) / 2)) return true;
+    for (int row = 0; row < num_rows; ++row) {
+        for (int col = 0; col <= row; ++col) {
+            if (visited[row][col]) continue;
+            if (in_degree[row][col]) continue;
+
+            visited[row][col] = true;
+            update_indegrees(num_rows, row, col, false, digraph, in_degree);
+            linear_extension[digraph_index(row, col)] = num_visited;
+            return find_linear_extension(num_rows, digraph, num_visited + 1, visited, in_degree, linear_extension);
+        }
+    }
+    return false;
+}
+
+int128_t sage_count_linear_extensions(int num_rows, uint64_t digraph) {
+    int in_degree[10][10];
+    bool visited[10][10];
+    int linear_extension[100];
+
+    for (int row = 0; row < num_rows; ++row) {
+        for (int col = 0; col <= row; ++col) {
+            in_degree[row][col] = 0;
+            visited[row][col] = false;
+        }
+    }
+
+    for (int row = 0; row < num_rows; ++row) {
+        for (int col = 0; col <= row; ++col) {
+            update_indegrees(num_rows, row, col, true, digraph, in_degree);
+        }
+    }
+
+    if (!find_linear_extension(num_rows, digraph, 0, visited, in_degree, linear_extension)) {
+        return 0;
+    }
+
+    int n = (num_rows * (num_rows + 1)) / 2;
+    vector<vector<int>> up(n, vector<int>{});
+
+    int num = 0;
+    for (int row = 0; row < num_rows - 1; ++row) {
+        for (int col = 0; col <= row; ++col) {
+            if (digraph & (1 << num)) {
+                up[linear_extension[num]].push_back(linear_extension[num + row + 1]);
+                up[linear_extension[num + row + 2]].push_back(linear_extension[num]);
+            }
+            else {
+                up[linear_extension[num]].push_back(linear_extension[num + row + 2]);
+                up[linear_extension[num + row + 1]].push_back(linear_extension[num]);
+            }
+            ++num;
+        }
+    }
+    
+    for (int i = n - 1; i >= 0; --i) {
+        set<int> new_up(up[i].begin(), up[i].end());
+        for (const int x : up[i]) {
+            new_up.insert(up[x].begin(), up[x].end());
+        }
+        up[i] = vector<int>(new_up.begin(), new_up.end());
+    }
+
+    unordered_map<int, vector<int>> Jup;
+    Jup[1] = vector<int>{};
+
+    vector<int> loc(n, 1);
+
+    int m = 1;
+
+    for (int x = 0; x < n; ++x) {
+        vector<vector<int>> K{{loc[x]}};
+        int j = 0;
+        while (K[j].size() > 0) {
+            vector<int> new_k;
+            for (const int a : K[j]) {
+                new_k.insert(new_k.end(), Jup[a].begin(), Jup[a].end());
+            }
+            K.push_back(new_k);
+            j += 1;
+        }
+        set<int> flat_k_set;
+        for (const vector<int>& sublist : K) {
+            flat_k_set.insert(sublist.begin(), sublist.end()); 
+        }
+        vector<int> flat_k = vector<int>(flat_k_set.begin(), flat_k_set.end());
+        for (int j = 0; j < flat_k.size(); ++j) {
+            int i = m + j + 1;
+            vector<int> jup_i;
+            jup_i.reserve(Jup[flat_k[j]].size());
+            for (const int a : Jup[flat_k[j]]) {
+                jup_i.push_back(m + find(flat_k.begin(), flat_k.end(), a) - flat_k.begin() + 1);
+            }
+            Jup[i] = jup_i;
+            Jup[flat_k[j]].push_back(i);
+        }
+        for (const int y : up[x]) {
+            loc[y] = find(flat_k.begin(), flat_k.end(), loc[y]) - flat_k.begin() + m + 1;
+        }
+        m += flat_k.size();
+    }
+    int128_t ct = 0;
+    unordered_map<int, int128_t> Jup_ct;
+    Jup_ct[m] = 1;
+    while (m > 1) {
+        m -= 1;
+        ct = 0;
+        for (const int j : Jup[m]) {
+            ct += Jup_ct[j];
+        }
+        Jup_ct[m] = ct;
+    }
+    return ct;
+}
+
 
 bool has_any_topological_orders(int num_rows, uint64_t digraph, int left_to_visit, bool visited[10][10], int in_degree[10][10]) {
     if (left_to_visit == 0) return true;
@@ -132,7 +252,8 @@ void digraph_thread(int num_rows, uint64_t start, uint64_t end, promise<int128_t
             result_promise.set_value(total);
             return;
         } 
-        total += count(num_rows, digraph);
+        //total += count(num_rows, digraph);
+        total += sage_count_linear_extensions(num_rows, digraph);
     }
 
     num_threads--;
