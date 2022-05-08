7064bc5e280cd9531eeeb9f35b22e30790b0d012
Dylan Nelson <nlsdyl001@myuct.ac.za>
2022-05-08 22:02:57 +0200

    don't track triangle itself and prefilter digraphs with no topological sorts

--- digraph.cpp
+++ digraph.cpp
@@ -18,7 +18,7 @@ constexpr int digraph_index(int row, int col) {
     return (row * (row + 1)) / 2 + col;
 }
 
-void update_indegrees(int num_rows, int row, int col, bool increment, uint64_t digraph, int (*in_degree)[10]) {
+void update_indegrees(int num_rows, int row, int col, bool increment, uint64_t digraph, int in_degree[10][10]) {
     int to_add = increment ? 1 : -1;
     if (row < num_rows - 1) {
         if (digraph & (1 << digraph_index(row, col))) {
@@ -42,7 +42,26 @@ void update_indegrees(int num_rows, int row, int col, bool increment, uint64_t d
     }
 }
 
-int128_t count(int num_rows, uint64_t digraph, int left_to_visit, bool (*visited)[10], int (*in_degree)[10], int (*triangle)[10]) {
+bool has_any_topological_orders(int num_rows, uint64_t digraph, int left_to_visit, bool visited[10][10], int in_degree[10][10]) {
+    if (left_to_visit == 0) return true;
+
+    for (int row = 0; row < num_rows; ++row) {
+        for (int col = 0; col <= row; ++col) {
+            if (visited[row][col]) continue;
+            if (in_degree[row][col] > 0) continue;
+
+            visited[row][col] = true;
+            update_indegrees(num_rows, row, col, false, digraph, in_degree);
+            bool result = has_any_topological_orders(num_rows, digraph, left_to_visit - 1, visited, in_degree);
+            update_indegrees(num_rows, row, col, true, digraph, in_degree);
+            visited[row][col] = false;
+            return result; 
+        }
+    }
+    return false;
+}
+
+int128_t count(int num_rows, uint64_t digraph, int left_to_visit, bool visited[10][10], int in_degree[10][10]) {
     if (left_to_visit == 0) {
         return 1;
     }
@@ -54,9 +73,8 @@ int128_t count(int num_rows, uint64_t digraph, int left_to_visit, bool (*visited
 
             visited[row][col] = true;
             update_indegrees(num_rows, row, col, false, digraph, in_degree);
-            triangle[row][col] = left_to_visit;
 
-            total += count(num_rows, digraph, left_to_visit - 1, visited, in_degree, triangle);
+            total += count(num_rows, digraph, left_to_visit - 1, visited, in_degree);
 
             update_indegrees(num_rows, row, col, true, digraph, in_degree);
             visited[row][col] = false;
@@ -69,7 +87,6 @@ int128_t count(int num_rows, uint64_t digraph, int left_to_visit, bool (*visited
 int128_t count(int num_rows, uint64_t digraph) {
     bool visited[10][10];
     int in_degree[10][10];
-    int triangle[10][10];
     for (int row = 0; row < num_rows; ++row) {
         for (int col = 0; col <= row; ++col) {
             visited[row][col] = false;
@@ -83,7 +100,10 @@ int128_t count(int num_rows, uint64_t digraph) {
         }
     }
 
-    return count(num_rows, digraph, (num_rows * (num_rows + 1)) / 2, visited, in_degree, triangle);
+    if (!has_any_topological_orders(num_rows, digraph, (num_rows * (num_rows + 1)) / 2, visited, in_degree)) {
+        return 0;
+    }
+    return count(num_rows, digraph, (num_rows * (num_rows + 1)) / 2, visited, in_degree);
 }
 
 void digraph_thread(int num_rows, uint64_t start, uint64_t end, promise<int128_t> result_promise) {
